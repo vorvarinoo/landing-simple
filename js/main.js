95678ce14375f64cb2d6b6e683c243974cae8a04
vorvarinoo <n.styrman@yandex.ru>
2023-12-12 02:08:28 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,35 @@
+import {
+  iosVhFix,
+} from './modules/utils.js';
+
+import {
+  validateForms,
+} from './modules/validate.js';
+
+import {
+  initModals,
+} from './modules/modal.js';
+
+import {
+  initSliders,
+} from './modules/slider.js';
+
+import {
+  initScrollTop,
+} from './modules/scroll-top.js';
+
+import {
+  burger,
+} from './modules/burger.js';
+
+document.addEventListener( 'DOMContentLoaded', () => {
+  iosVhFix();
+
+  window.addEventListener( 'load', () => {
+    validateForms();
+    initModals();
+    initSliders();
+    initScrollTop();
+    burger();
+  } );
+} );
