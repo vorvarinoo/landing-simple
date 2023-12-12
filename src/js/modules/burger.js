8cc91702f8a23fa6096f717e9f7477f5410f3e0d
vorvarinoo <n.styrman@yandex.ru>
2023-12-13 01:18:10 +0300

    Сверстал мобильное меню

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,5 +1,16 @@
-const burger = new JustBurger();
+const burger = new JustBurger( {
+  isOpen: ( burger ) => {
+    document.querySelector( '#overlay' ).classList.remove( 'hide' );
+    document.querySelector( '#overlay' ).classList.add( 'open' );
+    document.querySelector( 'html' ).classList.add( 'is-open-menu' );
+  },
+  isClose: ( burger ) => {
+    document.querySelector( '#overlay' ).classList.add( 'hide' );
+    document.querySelector( '#overlay' ).classList.remove( 'open' );
+    document.querySelector( 'html' ).classList.remove( 'is-open-menu' );
+  }
+} );
 
 export {
   burger,
-}
+};
